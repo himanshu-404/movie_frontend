5406789e44bf06b8e8221909db6a7985b0fd3b04
Himanshu <himanshurupavatiya04@gmail.com>
2023-09-15 15:56:44 +0530

    Config store redux-toolkit and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import WatchList from "./Pages/WatchList";
+import MovieDetails from "./Pages/MovieDetails";
 
 function App() {
   return (
-    <div>
-      <header>hello world</header>
+    <div className="App">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/watchlist" element={<WatchList />} />
+            <Route path="/:id" element={<MovieDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
